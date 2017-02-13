2f1f909fb4b73637118f8d7399589f36d26e0bdd
scgeier <scgeier@gmail.com>
2017-02-13 17:04:59 -0500

    Built a Web Audio version of animation sound FX (HTML5 wasn't fast enough on Safari)

--- js/scripts.js
+++ js/scripts.js
@@ -1,122 +1,123 @@
 $(document).ready(function(){
+	console.log("scripts.js is loaded");
+  $(window).load(function() {
+		$(".loader-wheel").fadeToggle();
+        //$(".container-fluid").toggle();
+		
+//Build the BufferLoader for the Web Audio API//
+function BufferLoader(context, urlList, callback) {
+  this.context = context;
+  this.urlList = urlList;
+  this.onload = callback;
+  this.bufferList = new Array();
+  this.loadCount = 0;
+}
 
-  if (window.innerWidth > 1024) {
+BufferLoader.prototype.loadBuffer = function(url, index) {
+  // Load buffer asynchronously
+  var request = new XMLHttpRequest();
+  request.open("GET", url, true);
+  request.responseType = "arraybuffer";
 
- //Load the sound FX//
+  var loader = this;
 
-function playDing() {
-      var sound = document.getElementById("ding");
-          sound.load();
-          sound.play();
-    };
+  request.onload = function() {
+    // Asynchronously decode the audio file data in request.response
+    loader.context.decodeAudioData(
+      request.response,
+      function(buffer) {
+        if (!buffer) {
+          alert('error decoding file data: ' + url);
+          return;
+        }
+        loader.bufferList[index] = buffer;
+        if (++loader.loadCount == loader.urlList.length)
+          loader.onload(loader.bufferList);
+      },
+      function(error) {
+        console.error('decodeAudioData error', error);
+      }
+    );
+  }
 
-function playRubber() {
-      var sound = document.getElementById("rubber");
-          sound.load();
-          sound.play();
-    };
-    
-function playTypewriter() {
-      var sound = document.getElementById("typewriter");
-          sound.load();
-          sound.play();
-    };
+  request.onerror = function() {
+    alert('BufferLoader: XHR error');
+  }
+
+  request.send();
+}
+
+BufferLoader.prototype.load = function() {
+  for (var i = 0; i < this.urlList.length; ++i)
+  this.loadBuffer(this.urlList[i], i);
+}
+
+
+//Load the sound FX//
+var context;
+var bufferLoader;
+  window.AudioContext = window.AudioContext || window.webkitAudioContext;
+      context = new AudioContext();
+
+    bufferLoader = new BufferLoader(
+    context,
+    [
+      '../audio/rubber-sound_mixdown.mp3',
+      '../audio/ding.wav',
+	  '../audio/typewriter.wav'
+    ],
+    finishedLoading
+    );
+
+  bufferLoader.load();
+
+var rubber;
+var ding;
+var typewriter;
+
+function finishedLoading(bufferList) {
+  // Create the sound sources and connect them to the speakers//
+  var rubber = context.createBufferSource();
+  var ding = context.createBufferSource();
+  var typewriter = context.createBufferSource();
+  var typewriter2 = context.createBufferSource();//you have to make a separate buffer source for each click//
+  var typewriter3 = context.createBufferSource();
+  var typewriter4 = context.createBufferSource();
+  var typewriter5 = context.createBufferSource();
+  var typewriter6 = context.createBufferSource();
+  var typewriter7 = context.createBufferSource();
+  
+  rubber.buffer = bufferList[0];
+  ding.buffer = bufferList[1];
+  typewriter.buffer = bufferList[2];
+  typewriter2.buffer = bufferList[2];
+  typewriter3.buffer = bufferList[2];
+  typewriter4.buffer = bufferList[2];
+  typewriter5.buffer = bufferList[2];
+  typewriter6.buffer = bufferList[2];
+  typewriter7.buffer = bufferList[2];
+  
+  rubber.connect(context.destination);
+  ding.connect(context.destination);
+  typewriter.connect(context.destination);
+  typewriter2.connect(context.destination);
+  typewriter3.connect(context.destination);
+  typewriter4.connect(context.destination);
+  typewriter5.connect(context.destination);
+  typewriter6.connect(context.destination);
+  typewriter7.connect(context.destination);
+  //rubber.start(0);
+  //source2.start(0);
+  //source3.start(0);
+
+
 
 //Change the I and M to red//
 function makeRed() {
     setTimeout(function(){
      document.getElementById("i").style.cssText = "color:red; font-size:1.2em; letter-spacing:2px; transition:0.2s linear;";
      document.getElementById("m").style.cssText = "color:red; font-size:1.2em; letter-spacing:2px; transition:0.2s linear;";
-	 playRubber();
-	 console.log(rubber);
-	 
- }, 1000);
-};
-
-//Add the apostrophe to make 'I'm' //
-function addApostrophe() {
-     setTimeout(function(){
-     document.getElementById("apostrophe").style.cssText = "display:inline; transition:0.5s linear;";
-     playDing();
-	
- }, 1800);
-};
-
-//Show the menu//
-function showMenu() {
-     setTimeout(function(){
-      document.getElementById("web").innerHTML = "<a href='#top'>web</a>";
-      playTypewriter();
-	 
- }, 2400);
-      setTimeout(function(){
-      document.getElementById("writing").innerHTML = "<a href='#top'>writing</a>";
-     playTypewriter();
-	
- }, 2600);
-      setTimeout(function(){
-      document.getElementById("graphics").innerHTML = "<a href='#top'>graphics</a>";
-      playTypewriter();
-	
- }, 2800);
-      setTimeout(function(){
-      document.getElementById("audio").innerHTML = "<a href='#top'>audio</a>";
-      playTypewriter();
-
- }, 3000);
-      setTimeout(function(){
-      document.getElementById("photo").innerHTML = "<a href='#top'>photo</a>";
-      playTypewriter();
-	  
- }, 3200);
-      setTimeout(function(){
-      document.getElementById("video").innerHTML = "<a href='#top'>video</a>";
-      playTypewriter();
-	  
- }, 3400);
-      setTimeout(function(){
-       document.getElementById("about").innerHTML = "about";
-       playTypewriter();
-	   
- }, 4500);
-};//showMenu();
-
-makeRed();
-addApostrophe();
-showMenu();
-}else{
-  console.log("mobile detected!");
-    $(window).load(function() {
-		$(".loader-wheel").toggle();
-        //$(".container-fluid").toggle();
-      
-//Load the sound FX//
-
-function playDing() {
-      var sound = document.getElementById("ding");
-          sound.load();
-          sound.play();
-    };
-
-function playRubber() {
-      var sound = document.getElementById("rubber");
-          sound.load();
-          sound.play();
-    };
-    
-function playTypewriter() {
-      var sound = document.getElementById("typewriter");
-          sound.load();
-          sound.play();
-    };
-
-
-
-//Change the I and M to red; only include the sound FX on desktop (they won't automatically play on iOS, anyway)//
-function makeRed() {
-    setTimeout(function(){
-     document.getElementById("i").style.cssText = "color:red; font-size:1.2em; letter-spacing:2px; transition:0.2s linear;";
-     document.getElementById("m").style.cssText = "color:red; font-size:1.2em; letter-spacing:2px; transition:0.2s linear;";
+	 rubber.start(0);
  }, 1000);
   };
 
@@ -124,6 +125,7 @@ function makeRed() {
 function addApostrophe() {
     setTimeout(function(){
      document.getElementById("apostrophe").style.cssText = "display:inline; transition:0.5s linear;";
+	 ding.start(0);
 	}, 1800);
 };
 
@@ -131,32 +133,39 @@ function addApostrophe() {
 function showMenu() {
      setTimeout(function(){
       document.getElementById("web").innerHTML = "<a href='#top'>web</a>";
+	  typewriter.start(0);
  }, 2400);
       setTimeout(function(){
       document.getElementById("writing").innerHTML = "<a href='#top'>writing</a>";
+	  typewriter2.start(0);
  }, 2600);
       setTimeout(function(){
       document.getElementById("graphics").innerHTML = "<a href='#top'>graphics</a>";
+	  typewriter3.start(0);
  }, 2800);
       setTimeout(function(){
       document.getElementById("audio").innerHTML = "<a href='#top'>audio</a>";
+	  typewriter4.start(0);
  }, 3000);
       setTimeout(function(){
       document.getElementById("photo").innerHTML = "<a href='#top'>photo</a>";
+	  typewriter5.start(0);
  }, 3200);
       setTimeout(function(){
       document.getElementById("video").innerHTML = "<a href='#top'>video</a>";
+	  typewriter6.start(0);
  }, 3400);
       setTimeout(function(){
        document.getElementById("about").innerHTML = "about";
+	   typewriter7.start(0);
  }, 4500);
 };//showMenu();
 
 makeRed();
 addApostrophe();
 showMenu();
+};
 
-console.log("scripts.js is loaded");
 
 
 $('.bxslider').bxSlider({
@@ -171,6 +180,5 @@ $('.bxslider').bxSlider({
         
  });//.load();
 	
-};
 
 });//.ready()
